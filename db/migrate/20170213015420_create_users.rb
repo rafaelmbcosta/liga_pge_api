72e8d02fa7137b5dccc90076ae4e0125fb098b43
Rafael Costa <rafaelmbcosta@gmail.com>
2017-03-13 01:19:10 -0300

    auth with knock configuration

--- db/migrate/20170213015420_create_users.rb
+++ db/migrate/20170213015420_create_users.rb
@@ -1,12 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :auth0_id
+      t.string :email, required: true
+      t.string :password_digest
 
       t.timestamps
     end
     add_index :users, :email, unique: true
-    add_index :users, :auth0_id, unique: true
   end
 end
