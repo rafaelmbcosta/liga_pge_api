7293649785c04e2030ab6adfefd4fbd6ce8e7182
Rafael Costa <rafael.costa@pge.ce.gov.br>
2017-05-25 16:40:17 -0300

    Team and player services

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,51 +1,56 @@
-class Api::V1::PlayersController < ApplicationController
-  before_action :set_player, only: [:show, :update, :destroy]
+module Api
+  module V1
+    class PlayersController < ApplicationController
+      before_action :authenticate_user, except: [ :index ]
+      before_action :set_player, only: [:show, :update, :destroy]
 
-  # GET /players
-  def index
-    @players = Player.all
+      # GET /players
+      def index
+        @players = Player.all
 
-    render json: @players
-  end
+        render json: @players
+      end
 
-  # GET /players/1
-  def show
-    render json: @player
-  end
+      # GET /players/1
+      def show
+        render json: @player
+      end
 
-  # POST /players
-  def create
-    @player = Player.new(player_params)
+      # POST /players
+      def create
+        @player = Player.new(player_params)
 
-    if @player.save
-      render json: @player, status: :created, location: @player
-    else
-      render json: @player.errors, status: :unprocessable_entity
+        if @player.save
+          render json: @player, status: :created, location: @player
+        else
+          render json: @player.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /players/1
+      def update
+        if @player.update(player_params)
+          render json: @player
+        else
+          render json: @player.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /players/1
+      def destroy
+        @player.destroy
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_player
+          @player = Player.find(params[:id])
+        end
+
+        # Only allow a trusted parameter "white list" through.
+        def player_params
+          params.require(:player).permit(:name, :active)
+        end
     end
   end
-
-  # PATCH/PUT /players/1
-  def update
-    if @player.update(player_params)
-      render json: @player
-    else
-      render json: @player.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /players/1
-  def destroy
-    @player.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_player
-      @player = Player.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def player_params
-      params.require(:player).permit(:name, :active)
-    end
 end
---
+++ spec/factories/api/v1/player.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :v1_player, class: Api::V1::Player do
+    name    { Faker::Name.name }
+    active  { true }
+  end
+end
