20fa070291010d8bb58bb58616d138163e16efd3
FABIO GURGEL <fabio.gurgel@pge.ce.gov.br>
2017-06-27 10:20:06 -0300

    add proxy to class connection

--- lib/web/api_cartola/connection.rb
+++ lib/web/api_cartola/connection.rb
@@ -1,16 +1,23 @@
 #encoding:utf-8
 require 'net/http'
+require 'open-uri'
 module Web
   module ApiCartola
     class Connection
 
-      URL_TEAM_SCORE = 'https://api.cartolafc.globo.com/time/slug/'
+      URL_TEAM_SCORE = "https://api.cartolafc.globo.com/time/slug/"
+      # URL_TEAM_SCORE = "https://api.cartolafc.globo.com/time/slug/fabayern-fc/5"
 
-      def self.get_team_score(slug, round)
-        uri = URI(URL_TEAM_SCORE + slug + '/' + round)
-        return Net::HTTP.get(uri)
+      def self.get_team_score(slug, round=nil)
+        uri = URI(URL_TEAM_SCORE + "#{slug}")
+        uri = URI(URL_TEAM_SCORE + "#{slug}/#{round}") unless round.nil?
+
+        # http = Net::HTTP::Proxy(@proxy_host, @proxy_port, @proxy_user, @proxy_pass)
+        http = Net::HTTP::Proxy('proxy', '3128', 'fabi', 'passwd')
+        request = http.get_response(uri)
+        return request.body
+
       end
-
     end
   end
 end
