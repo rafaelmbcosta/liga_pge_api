35269fc43e4ea560117ecb20d1fdb2d2bee000a7
Rafael Costa <rafael.costa@pge.ce.gov.br>
2017-07-21 10:15:10 -0300

    Generating brackets

--- app/models/api/v1/round.rb
+++ app/models/api/v1/round.rb
@@ -1,3 +1,45 @@
-class Round < ApplicationRecord
-  belongs_to :season
+module Api
+  module V1
+    class Round < ApplicationRecord
+      belongs_to :season
+
+      def self.battle_generator
+        fantasma = Player.where("name = 'Fantasma'").first
+        round = Round.new
+        last_round = Round.last
+        round.number = last_round.number + 1
+        round.season_id = Season.last.id
+        round.date = Time.now
+        round.save
+
+        players = Player.where("active is true").to_a
+        unless ( players.size % 2 == 0 )
+          players.delete(fantasma)
+        end
+
+        while ( players.size > 0 ) do
+          player = players[rand(players.size)]
+          # Comparo o player com todos os outros players
+          confrontos = Hash.new
+          players.delete(player)
+          players.each do |rival|
+            home = Battle.where("first_id = ? and second_id = ?", player.id, rival.id)
+            visiting = Battle.where("first_id = ? and second_id = ?", rival.id, player.id)
+            confrontos[rival.id] = home.size + visiting.size
+          end
+
+          #descobre os que tem menos jogos
+          menor = 10
+          confrontos.each do |k, v|
+            menor = v if v < menor
+          end
+          #agora pega os que tem menos jogos
+          adversarios = confrontos.select{|k,v| v == menor }.collect{|k, v| k}
+          adversario = Player.find(adversarios[rand(adversarios.size)])
+          players.delete(adversario)
+          Battle.create(first_id: player.id, second_id: adversario.id, round_id: round.id )
+        end
+      end
+    end
+  end
 end
--- db/migrate/20170211125041_create_teams.rb
+++ db/migrate/20170211125041_create_teams.rb
@@ -3,6 +3,11 @@ class CreateTeams < ActiveRecord::Migration[5.0]
     create_table :teams do |t|
       t.string :name, null: false
       t.boolean :active, null: false, default: true
+      # Dados do site
+      t.string :slug
+      t.string :url_escudo_png
+      # adicionar aqui todos os dados que vem da API, e deixa pra na view atualizar o cadastro
+      # correr na aula para chegar nos forms
       t.references :season, foreign_key: true
       t.references :player, foreign_key: true
 
