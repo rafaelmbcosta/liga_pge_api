acf76a01df02c2712848c754cab6ac91f5423a48
Rafael Costa <rafaelmbcosta@gmail.com>
2019-05-09 23:52:03 -0300

    Preparations

--- app/workers/api/v1/team_worker.rb
+++ app/workers/api/v1/team_worker.rb
@@ -3,7 +3,9 @@ module Api
     class TeamWorker
       def self.perform
         Team.where(season: Season.active).each do |team|
-          team.update_attributes(url_escudo_png: Connection.team_score(team.slug)["time"]["url_escudo_svg"])
+          team_data = Connection.team_score(team.slug)
+          raise "ERRO TEAM ID: #{team.id}, SLUG: (#{team.slug})" if team_data.nil?
+          team.update_attributes(url_escudo_png: team_data["time"]["url_escudo_svg"])
         end
       end
     end
