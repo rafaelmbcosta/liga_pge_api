b093f9bfa1a435d9d8767b59341a305745166d0e
Rafael Costa <rafaelmbcosta@gmail.com>
2019-05-09 23:42:26 -0300

    Preparations

--- app/workers/api/v1/team_worker.rb
+++ app/workers/api/v1/team_worker.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class TeamWorker
       def self.perform
-        Team.where(season: Season.last).each do |team|
+        Team.where(season: Season.active).each do |team|
           team.update_attributes(url_escudo_png: Connection.team_score(team.slug)["time"]["url_escudo_svg"])
         end
       end
