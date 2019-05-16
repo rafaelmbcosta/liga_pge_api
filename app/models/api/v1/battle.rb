39b2884d921c385a2a405c3901fea387a6026567
Rafael Costa <rafaelcosta@16935L.pge.ce.gov.br>
2019-05-16 17:34:43 -0300

    refatoring battles

--- app/models/api/v1/battle.rb
+++ app/models/api/v1/battle.rb
@@ -3,6 +3,10 @@ module Api
     class Battle < ApplicationRecord
       belongs_to :round
 
+      scope :find_battle, ->(round_id, team, other_team){
+        where(round: round_id).where('first_id = ? and second_id = ?', team, other_team)
+      }
+
       def self.find_ghost_battle(round_id)
         Battle.where(round_id: round_id).select { |battle| battle.first_id.nil? || battle.second_id.nil? }
       end
@@ -11,42 +15,55 @@ module Api
         Round.avaliable_for_battles
       end
 
+      def self.check_encounters(team, teams, battle_history, round)
+        battle_history = Hash.new
+        teams.each do |rival|
+          home = Battle.find_battle(round, team, rival)
+          visiting = Battle.find_battle(round, rival, team)
+          battle_history[rival] = home.size + visiting.size
+        end
+        battle_history
+      end
 
+      def self.less_battle_number(battle_history)
+        less = 38
+        battle_history.each do |_rival, number|
+          less = number if number < less
+        end
+        less
+      end
+
+      def self.sort_rival(battle_history, less, teams)
+        rivals = battle_history.select { |_rival, number| number == less } 
+                               .collect { |rival, _number| rival}
+        rival = rivals[rand(rivals.size)]
+        teams.delete(rival)
+        return rival, teams
+      end
+
+      def create_battle(team, rival, round)
+        Battle.create(first_id: team, second_id: rival, round: round)
+      end
+
+      def pick_team(teams)
+        team = teams[rand(teams.size)]
+        teams.delete(team)
+        return team, teams
+      end
+
+      def self.sort_battle(teams, round)
+        while (teams.size > 0) do
+          chosen_team, teams = pick_team(teams)
+          battle_history = check_encounters(chosen_team, teams, battle_history, round)
+          less_battle_number = less_battle_number(battle_history)
+          rival, teams = sort_rival(battle_history, less, teams)
+          create_battle(chosen_team, rival, round)
+        end
+      end
 
       def self.generate_battles(round)
-        # bloco só para saber se teremos ou nao fantasma
-        # teams = Team.active.pluck(:id)
-        ghost = Team.ghost_needed?
         teams = Team.new_battle_teams
-
-        fantasma = false
-        if !(teams.size % 2 == 0)
-          fantasma = true
-          teams << nil
-        end
-
-        while (teams.size > 0) do
-          team = teams[rand(teams.size)]
-          # Comparo o player com todos os outros players
-          confrontos = Hash.new
-          teams.delete(team)
-          teams.each do |rival|
-            home = Battle.where("first_id = ? and second_id = ?", team, rival)
-            visiting = Battle.where("first_id = ? and second_id = ?", rival, team)
-            confrontos[rival] = home.size + visiting.size
-          end
-
-          #descobre os que tem menos jogos
-          menor = 38
-          confrontos.each do |k, v|
-            menor = v if v < menor
-          end
-          # agora pega os que tem menos jogos
-          adversarios = confrontos.select{|k,v| v == menor }.collect{|k, v| k}
-          adversario = adversarios[rand(adversarios.size)]
-          teams.delete(adversario)
-          Battle.create(first_id: team, second_id: adversario, round_id: round.id )
-        end
+        sort_battle(team, round)
       end
 
       def self.create_battles
