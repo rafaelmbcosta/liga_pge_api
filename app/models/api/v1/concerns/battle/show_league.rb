67e84e13e9ef5d9a675614cdeeb3e4ecbe11d263
Rafael Costa <rafael.costa@pge.ce.gov.br>
2019-07-19 17:59:07 -0300

    More tests

--- app/models/api/v1/concerns/battle/show_league.rb
+++ app/models/api/v1/concerns/battle/show_league.rb
@@ -8,24 +8,27 @@ module Api
 
           included do
             # returns the league report for each dispute month
-            def self.dispute_month_league_report(dm, teams)
-              league = { name: dm.name, id: dm.id, players: [] }
-              league[:players] = league_report_teams(teams, dm.battles) if dm.battles.any?
+            def self.dispute_month_league_report(dispute, teams)
+              league = { name: dispute.name, id: dispute.id, players: [] }
+              league[:players] = league_report_teams(teams, dispute.battles) if dispute.battles.any?
               league
             end
 
             # find the opponent name or return 'ghost'
             def self.opponent(battle, team, teams)
               opponent_id = battle.first_id == team.id ? battle.second_id : battle.first_id
-              return opponent_id.nil? ? 'Fantasma' : "#{teams.find(opponent_id).name}"
+              opponent_id.nil? ? 'Fantasma' : teams.find(opponent_id).name.to_s
             end
 
             def self.league_battle_result(battle, team)
               return [1, 0] if battle.draw
+
               first_diff = battle.first_points - battle.second_points
               return [3, first_diff] if battle.first_win && battle.first_id == team.id
+
               second_diff = battle.second_points - battle.first_points
               return [3, second_diff] if battle.second_win && battle.second_id == team.id
+              
               [0, 0]
             end
 
@@ -45,8 +48,8 @@ module Api
               teams.active.each do |team|
                 team_battles = battles.select { |battle| battle.first_id == team.id || 
                                                          battle.second_id == team.id }
-                team_details = league_team_details(teams, team, team_battles)
-                data = { name: team.player_name, team: team.name, id: id,
+                team_details = team_details(teams, team, team_battles)
+                data = { name: team.player_name, team: team.name, id: team.id,
                          team_symbol: team.url_escudo_png, details: team_details,
                          points: team_details.pluck(:points).sum,
                          diff_points: team_details.pluck(:diff_points).sum }
--- spec/support/concerns/show_league.rb
+++ spec/support/concerns/show_league.rb
@@ -8,7 +8,6 @@ module Api
       let(:dispute_month) { FactoryBot.create(:v1_dispute_month, season: season) }
 
       describe 'show_league' do
-
         before do
           season.dispute_months << dispute_month
           allow(Season).to receive(:active).and_return(season)
@@ -55,21 +54,21 @@ module Api
         let(:team) { FactoryBot.create(:v1_team) }
         let(:second_team) { FactoryBot.create(:v1_team) }
 
-        let(:battle) do 
-          Battle.new(first_id: team.id, second_id: second_team.id, 
+        let(:battle) do
+          Battle.new(first_id: team.id, second_id: second_team.id,
                      first_points: 10, second_points: 4, draw: false,
                      first_win: false, second_win: false)
         end
 
-        let(:second_battle) do 
-          Battle.new(first_id: second_team.id, second_id: team.id, 
+        let(:second_battle) do
+          Battle.new(first_id: second_team.id, second_id: team.id,
                      first_points: 4, second_points: 10, draw: false,
                      first_win: false, second_win: true)
         end
 
         it 'returns [1,0] if its a draw' do
           battle.draw = true
-          expect(model.league_battle_result(battle, team)).to eq([1,0])
+          expect(model.league_battle_result(battle, team)).to eq([1, 0])
         end
 
         it 'returns [3, difference] in case of win and he its the first team' do
@@ -91,8 +90,8 @@ module Api
         let(:team) { FactoryBot.create(:v1_team) }
         let(:second_team) { FactoryBot.create(:v1_team) }
 
-        let(:battle) do 
-          Battle.new(first_id: team.id, second_id: second_team.id, 
+        let(:battle) do
+          Battle.new(first_id: team.id, second_id: second_team.id,
                      first_points: 10, second_points: 4, draw: false,
                      first_win: false, second_win: false, round: round)
         end
@@ -102,7 +101,7 @@ module Api
         let(:expectation) do
           [
             {
-              round: 2, points: 6, diff_points: 70, 
+              round: 2, points: 6, diff_points: 70,
               opponent: second_team.name
             }
           ]
@@ -118,8 +117,40 @@ module Api
           expect(model.team_details([team, second_team], team, [])).to eq([])
         end
 
-        it  'returns array of details if it succeeds' do
+        it 'returns array of details if it succeeds' do
           expect(model.team_details([team, second_team], team, [battle])).to eq(expectation)
+        end
+      end
+
+      describe 'league_report_teams' do
+        let(:team) { FactoryBot.create(:v1_team) }
+
+        let(:battle) do
+          Battle.new(first_id: team.id, second_id: nil,
+                     first_points: 10, second_points: 4, draw: false,
+                     first_win: false, second_win: false, round: round)
+        end
+
+        let(:teams) { [team] }
+        let(:team_details) { [{ diff_points: 7, points: 2 }] }
+
+        let(:expectation) do
+          [
+            {
+              name: team.player_name, team: team.name, id: team.id,
+              team_symbol: team.url_escudo_png, details: team_details,
+              points: 2, diff_points: 7
+            }
+          ]
+        end
+
+        before do
+          allow(team).to receive(:active).and_return(teams)
+          allow(model).to receive(:team_details).and_return(team_details)
+        end
+
+        it 'returns the team details' do
+          expect(model.league_report_teams([battle], team)).to eq(expectation)
         end
       end
     end
