c3efa2d035cec044bbb83e4c38ee6890b698ef68
Rafael Costa <rafael.costa@pge.ce.gov.br>
2019-08-06 09:23:13 -0300

    setting up front end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://liga-pge.herokuapp.com']
+    origins ['http://liga-pge.herokuapp.com', 'http://liga-pge-adm.herokuapp.com']
     resource '*',
       headers: :any,
       methods: [:get, :post]
