b59cc62b1443aa7330842eaa9cc0288ab13df7f2
Rafael Costa <rafael.costa@pge.ce.gov.br>
2019-08-21 10:13:36 -0300

    fix

--- app/controllers/api/v1/battles_controller.rb
+++ app/controllers/api/v1/battles_controller.rb
@@ -1,13 +1,14 @@
 module Api
   module V1
-  class BattlesController < ApplicationController
-    before_action :authenticate_user, except: [ :index ]
+    class BattlesController < ApplicationController
+      before_action :authenticate_user, except: [ :index ]
 
-    # GET /battles
-    def index
-      @battles = Battle.list_battles
+      # GET /battles
+      def index
+        @battles = Battle.list_battles
 
-      render json: @battles
+        render json: @battles
+      end
     end
   end
 end
