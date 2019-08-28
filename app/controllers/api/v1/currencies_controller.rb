67ef8304c59c3f0901bf079b42842d7bffca0546
Rafael Costa <rafaelmbcosta@gmail.com>
2019-08-28 00:51:09 -0300

    new routes

--- app/controllers/api/v1/currencies_controller.rb
+++ app/controllers/api/v1/currencies_controller.rb
@@ -1,18 +1,18 @@
 module Api
   module V1
     class CurrenciesController < ApplicationController
-      before_action :authenticate_user, except: [ :index ]
+      before_action :authenticate_user, except: [:index]
 
       # GET /currencies
       def index
-        @currencies = $redis.get("currencies")
+        @currencies = $redis.get('currencies')
 
         render json: @currencies
       end
 
       def rerun
         Currency.rerun_currencies
-        render json: { mensagem: 'ok'}
+        render json: { mensagem: 'ok' }
       end
     end
   end
