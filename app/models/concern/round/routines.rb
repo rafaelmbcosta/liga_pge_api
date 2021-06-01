809d3869387f08c85fd564c2937ed94db9720d14
Rafael Costa <rafaelmbcosta@gmail.com>
2021-06-01 02:41:16 -0300

    refactoring

---
+++ app/models/concern/round/routines.rb
@@ -0,0 +1,29 @@
+require 'active_support/concern'
+
+module Concern::Round::Routines
+  extend ActiveSupport::Concern
+
+  included do
+    def self.general_tasks_routine
+      RoundWorker.perform
+      # TeamWorker.perform
+      # SeasonWorker.perform("season_finished")
+    end
+
+    def self.closed_market_routines(round_id = nil)
+      # BattleWorker.perform("closed_market")
+      # ScoresWorker.perform("closed_market")
+      # pipeline = Pipeline::Pipe.new({round_id: round_id})
+      # pipeline.stage(Pipeline::Battle::StageBattleWorker.new())
+      # pipeline.stage(Pipeline::Scores::StageScoresWorker.new())
+      # pipeline.executa()
+    end
+
+    def self.round_finished_routines
+      # ScoresWorker.perform('finished_round')
+      # BattleWorker.perform('finished_round')
+      # CurrencyWorker.perform
+      # SeasonWorker.perform("finished_round")
+    end
+  end
+end
---
+++ spec/models/season_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe Season, :type => :model do
+  before do
+    DatabaseCleaner.start
+    DatabaseCleaner.clean
+  end
+
+  describe "Validations" do
+    it { should have_many(:dispute_months) }
+    it { should have_many(:rounds) }
+    it { should have_many(:scores) }
+    it { should validate_presence_of(:year) }
+  end
+
+  describe 'only_one_active after create hook' do
+    let(:seasons) { [
+      { year: 2020, finished: false},
+      { year: 2021, finished: false}
+    ] }
+
+    it 'dont raise any error if ok' do
+      expect(Season.create(seasons.first)).to be_instance_of Season
+    end
+
+    it 'raise error if there is another active' do
+      expect{ Season.create(seasons) }.to raise_error 'Season already exist'
+    end
+  end
+end
