891f34e35003b1576a9bf7f6a9171a0e3a52ef19
Rafael Costa <rafaelmbcosta@gmail.com>
2021-06-27 17:59:45 -0300

    fixing league route

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,13 @@ Rails.application.routes.draw do
       get 'teams', to: 'teams#index'
       get 'teams/find_team', to: 'teams#find_team'
       post 'teams', to: 'teams#create'
+      get 'league', to: 'dispute_months#league_points'
       post 'teams/activation', to: 'teams#activation'
     end
   end
   get 'championship', to: 'awards#championship'
   get 'currencies', to: 'currencies#index'
   get 'currencies/rerun', to: 'currencies#rerun'
-  get 'league', to: 'dispute_months#league_points'
   get 'season_dispute_months', to: 'dispute_months#active_rounds'
   get 'monthly', to: 'awards#monthly'
   get 'season_score', to: 'seasons#scores'
